af8fd3c631afe45869d0db36cdacb8984766de90
Thomas Auinger <thomas@auinger.de>
2022-06-24 11:53:11 +0200

    added text to InstanceOfTest

--- basic/src/test/java/katas/oop/InstanceOfTest.java
+++ basic/src/test/java/katas/oop/InstanceOfTest.java
@@ -5,6 +5,16 @@ import org.junit.jupiter.api.Test;
 
 public class InstanceOfTest {
 
+    /**
+     * We will use OOP practices to instantiate a Cat and a Shark and check that they are of the correct types.
+     *
+     * The type hierarchy is expected to be like this:
+     *
+     * Vertebrate --> Mammal --> Cat
+     *            \-> Fish --> Shark
+     *
+     * Each vertebrate has a name.
+     */
     @Test
     void instanceOfTest() {
         // a cat is a mammal and a vertebrate
@@ -12,15 +22,17 @@ public class InstanceOfTest {
         Assertions.assertThat(cat).isInstanceOf(Mammal.class);
         Assertions.assertThat(cat).isInstanceOf(Vertebrate.class);
 
-        // each mammal has a name -- TODO uncomment the next two lines
-//        Mammal mammal = cat;
-//        Assertions.assertThat(mammal.getName()).isEqualTo("Niko");
+        // each vertebrate has a name -- TODO uncomment the next two lines
+//        Vertebrate vertebrate = cat;
+//        Assertions.assertThat(vertebrate.getName()).isEqualTo("Niko");
 
         // a shark is a fish and a vertebrate but never a mammal
         Shark shark = new Shark("Bruce");
         Assertions.assertThat(shark).isInstanceOf(Fish.class);
         Assertions.assertThat(shark).isInstanceOf(Shark.class);
         Assertions.assertThat(shark).isNotInstanceOf(Mammal.class);
+
+        // TODO feel free to add a name check for the shark!
     }
 }
 
